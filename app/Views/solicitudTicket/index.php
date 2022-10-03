a78819aac82bde1379830078f63376e1c742369e
edgardegantea <edgar.degante.a@gmail.com>
2022-10-03 17:08:26 -0500

    first commit

---
+++ app/Views/solicitudTicket/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tickets de soporte</title>
+</head>
+<body>
+	Tickets de soporte de Ingeniería Informática
+
+	<h5>
+		<a href="<?php echo base_url('solicitud/new'); ?>">
+			Nuevo ticket
+		</a>
+	</h5>
+</body>
+</html>
